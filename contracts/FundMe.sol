11bb8099c5f11bd2049edd9d26db6f9565006581
Jon-Fredrik Hopland <jho107@post.uit.no>
2022-07-10 16:58:47 +0200

    updated contract

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,53 +1,60 @@
 // SPDX-License-Identifier: MIT
-
-// get funds from users
-// withraw funds
-// set a minimum funding value in USD
-
-pragma solidity ^0.8.8;
-
+// 1. Pragma
+pragma solidity ^0.8.0;
+// 2. Imports
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
+// 3. Interfaces, Libraries, Contracts
 error FundMe__NotOwner();
 
-/** @title A contract for crowd funding
- * @author Jon-Fredrik Hopland
- * @notice This contract is to demo a sample funding contract
+/**@title A sample Funding Contract
+ * @author Joffen Hopland
+ * @notice This contract is for creating a sample funding contract
  * @dev This implements price feeds as our library
  */
 contract FundMe {
+    // Type Declarations
     using PriceConverter for uint256;
 
+    // State variables
+    uint256 public constant MINIMUM_USD = 50 * 10**18;
+    address private immutable i_owner;
     address[] private s_funders;
     mapping(address => uint256) private s_addressToAmountFunded;
-    address private immutable i_owner;
-    uint256 public constant MINIMUM_USD = 50 * 1e18;
     AggregatorV3Interface private s_priceFeed;
 
+    // Events (we have none!)
+
+    // Modifiers
     modifier onlyOwner() {
-        // require(msg.sender == i_owner, "Sender is not owner!");
-        if (msg.sender != i_owner) {
-            revert FundMe__NotOwner();
-        }
+        // require(msg.sender == i_owner);
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
         _;
     }
 
-    constructor(address s_priceFeedAddress) {
+    // Functions Order:
+    //// constructor
+    //// receive
+    //// fallback
+    //// external
+    //// public
+    //// internal
+    //// private
+    //// view / pure
+
+    constructor(address priceFeed) {
+        s_priceFeed = AggregatorV3Interface(priceFeed);
         i_owner = msg.sender;
-        s_priceFeed = AggregatorV3Interface(s_priceFeedAddress);
     }
 
-    /**
-     * @notice This function funds this contract
-     * @dev This implements price feeds as our library
-     */
-
+    /// @notice Funds our contract based on the ETH/USD price
     function fund() public payable {
         require(
             msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
             "You need to spend more ETH!"
         );
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
         s_addressToAmountFunded[msg.sender] += msg.value;
         s_funders.push(msg.sender);
     }
@@ -85,20 +92,28 @@ contract FundMe {
         require(success);
     }
 
-    function getOwner() public view returns (address) {
-        return i_owner;
+    /** @notice Gets the amount that an address has funded
+     *  @param fundingAddress the address of the funder
+     *  @return the amount funded
+     */
+    function getAddressToAmountFunded(address fundingAddress)
+        public
+        view
+        returns (uint256)
+    {
+        return s_addressToAmountFunded[fundingAddress];
+    }
+
+    function getVersion() public view returns (uint256) {
+        return s_priceFeed.version();
     }
 
     function getFunder(uint256 index) public view returns (address) {
         return s_funders[index];
     }
 
-    function getAddressToAmountFunded(address funder)
-        public
-        view
-        returns (uint256)
-    {
-        return s_addressToAmountFunded[funder];
+    function getOwner() public view returns (address) {
+        return i_owner;
     }
 
     function getPriceFeed() public view returns (AggregatorV3Interface) {
